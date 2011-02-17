9402ad84624983d0a3d5fbe0be3360ad61b43d9f
Erick Matsen <matsen@fhcrc.org>
2011-02-17 11:00:42 -0800

    added data

---
+++ demo.sh
@@ -0,0 +1,8 @@
+placeviz kr *
+mokaphy kr *place
+mokaphy kr --list-out *place
+mokaphy cluster *place
+mokaphy cluster -o my_cluster *place
+aptx my_cluster/cluster.tre 
+aptx mass_trees/0001.phy.fat.xml 
+placeutil --help
