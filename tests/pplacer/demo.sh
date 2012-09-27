11061ea723f8f8e98ff046991197d6794ba6162c
Aaron Gallagher <habnabit@gmail.com>
2012-09-26 22:58:02 -0700

    Whoops; this is running -u.

--- tests/pplacer/demo.sh
+++ tests/pplacer/demo.sh
@@ -28,7 +28,7 @@ pplacer --fantasy 0.1 $refpkg $SRC/p4z1r36.fasta
 pplacer --fantasy 0.2 $refpkg $SRC/p4z1r36.fasta
 
 # These tests can only be run without an $ALTSRC set.
-if [ "x$ALTSRC" = "x" ]; then
+if [ "x${ALTSRC:-}" = "x" ]; then
     refpkg_tree=$(taxit rp "$SRC/vaginal_16s.refpkg" tree)
     refpkg_stats=$(taxit rp "$SRC/vaginal_16s.refpkg" tree_stats)
     pplacer -t $refpkg_tree -s $refpkg_stats $SRC/p4z1r36.fasta
